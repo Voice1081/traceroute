e9e17eaf6957d64416fc44836559fbcb8ea2c921
Voice1081 <voice1081@gmail.com>
2019-03-30 13:09:21 +0500

    fixed

--- traceroute.py
+++ traceroute.py
@@ -1,10 +1,13 @@
 import subprocess
 import re
 import json
+import argparse
 from urllib.request import urlopen
+from urllib.error import URLError
 from collections import namedtuple
+import prettytable
 ip_regex = re.compile(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})')
-res_info = namedtuple('res_info', ['number', 'ip', 'as', 'country', 'isp'])
+res_info = namedtuple('res_info', ['number', 'ip', 'autonomous_system', 'country', 'isp'])
 
 
 def get_traceroute(addr):
@@ -14,8 +17,9 @@ def get_traceroute(addr):
     for line in output:
         l = line.split()
         if '*' in l: break
-        if ip_regex.match(l[-1]):
-            ips.append((l[0], l[-1]))
+        if len(l) != 0:
+            if ip_regex.match(l[-1]):
+                ips.append((l[0], l[-1]))
     return ips
 
 
@@ -24,7 +28,44 @@ def get_info(ips):
     for ip in ips:
         with urlopen('http://ip-api.com/json/{0}'.format(ip[1])) as response:
             data = json.load(response)
-            res = res_info(ip[0], ip[1], data['as'], data['country'], data['isp'])
+            if data['status'] != 'fail':
+                res = res_info(ip[0], ip[1], data['as'], data['country'], data['isp'])
+            else:
+                res = res_info(ip[0], ip[1], 'Not stated', 'Not stated', 'Not stated')
             result.append(res)
     return result
 
+
+def make_table(result):
+    table = prettytable.PrettyTable(['number', 'IP', 'Autonomous system', 'Country', 'Provider'])
+    for r in result:
+        table.add_row(list(r))
+    return table
+
+
+def traceroute(addr):
+    table = make_table(get_info(get_traceroute(addr)))
+    with open('{} traceroute.txt'.format(addr), 'w') as f:
+        f.write(table.get_string())
+
+
+def internet_on():
+    try:
+        urlopen('google.com', timeout=5)
+        return True
+    except URLError:
+        return False
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--address', '-a', action='store', dest='addr')
+    args = parser.parse_args()
+    if internet_on():
+        traceroute(args.addr)
+    else:
+        print('No connection')
+
+
+if __name__ == '__main__':
+    traceroute()
